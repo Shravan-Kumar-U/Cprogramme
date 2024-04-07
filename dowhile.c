b0b882b79ce97658147c527f1516bccb17c8d6d6
Shravan Kumar <kumarshravan12546@gmail.com>
2024-04-07 15:18:42 +0530

    first commit

---
+++ dowhile.c
@@ -0,0 +1,17 @@
+// do while loop
+// excecute the code and check the condition
+// do while loop wil run atleast one time
+
+# include<stdio.h>
+
+int main(){
+    int i=0;
+    do
+    {
+        /* code */printf("%d\n",i);
+        i++;
+    } while (/* condition */i<=10);
+    
+
+    return 0;
+}
