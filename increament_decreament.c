8b39cb821e3afe80d06c82786fa1545924f46926
Shravan Kumar <kumarshravan12546@gmail.com>
2024-04-07 09:59:57 +0530

    first commit

---
+++ increament_decreament.c
@@ -0,0 +1,23 @@
+// Increament and decreament operations
+// i++ -> i is increased by 1 i.e i = i+1
+// i-- -> i is decreased by 1 i.e i = i-1
+// int i=3;
+//     printf("The value after i++ is %d\n",i++);
+//     printf("The value of i is %d \n",i);
+// here output will be 3 and 4
+// i++ means pehle print karo phir increament karo
+// int i=3;
+//     printf("The value after ++i is %d\n",++i);
+//     printf("The value of i is %d \n",i);
+// here output will be 4 and 4
+// ++i means pehle inreament karo phir print karo(pehle isthamal karo phir vishwas kare)
+
+
+# include<stdio.h>
+int main(){
+    int i=3;
+    printf("%d\n",--i);
+    printf("%d",i);
+    return 0;
+
+}
