3826428aeeb43ec1b2a0b23eaf9549e3600c36e1
Shravan Kumar <kumarshravan12546@gmail.com>
2024-04-08 20:42:11 +0530

    natural number

---
+++ print_naturalnum.c
@@ -0,0 +1,15 @@
+# include<stdio.h>
+
+int main(){
+    int n;
+    printf("Enter the natural number :");
+    scanf("%d",&n);
+    int i=1;
+    do
+    {
+        printf("%d\n",i);
+        i++;
+        /* code */
+    } while (i<=n);
+    
+}
