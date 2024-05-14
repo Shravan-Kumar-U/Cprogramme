04d090b64377ea2561e5392e2e315b576c1b3d11
Shravan Kumar U <kumarshravan12546@gmail.com>
2024-05-14 15:16:10 +0530

    TO take  the input from user naive way

---
+++ totoakeinputinarray.c
@@ -0,0 +1,14 @@
+# include<stdio.h>
+
+int main(){
+    int arr[4];
+    printf("Enter  the value for index 1");
+    scanf("%d",&arr[0]);
+    printf("Enter  the value for index 1");
+    scanf("%d",&arr[1]);
+    printf("Enter  the value for index 1");
+    scanf("%d",&arr[2]);
+    printf("Enter  the value for index 1");
+    scanf("%d",&arr[3]);
+    return 0;
+}
