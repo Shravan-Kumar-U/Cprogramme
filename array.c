05451fa14e048b3261ca7cb92add42669b476337
Shravan Kumar U <kumarshravan12546@gmail.com>
2024-05-15 16:11:18 +0530

    it is a program to represent the array in c programming

---
+++ array.c
@@ -0,0 +1,10 @@
+# include<stdio.h>
+
+int main(){
+    int array[]={12,23,34,45};
+    printf("The value of a[0] is %d\n",array[0]);
+    printf("The value of a[1] is %d\n",array[1]);
+    printf("The value of a[2] is %d\n",array[2]);
+    printf("The value of a[3] is %d\n",array[3]);
+    return 0; 
+}
